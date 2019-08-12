43126f56b4d9ffe4b9f3cf5c74d9c147233cb64a
Lancelot OWCZARCZAK <lowczarc@student.42.fr>
2019-08-12 08:39:05 +0200

    Create NeuralNet and DCostNeuralNet struct

--- src/main.rs
+++ src/main.rs
@@ -1,119 +1,78 @@
+mod neural_network;
 mod read_idx;
 
+const STEP_SIZE: usize = 50;
+
+use neural_network::AverageDCost;
+
 use rand::prelude::*;
-
-fn sigmoid(x: f64) -> f64 {
-    1. / (1. + (-x).exp())
-}
-
-fn sigmoid_derivative(x: f64) -> f64 {
-    let tmp = (-x).exp();
-    tmp / (1. + tmp).powi(2)
-}
-
-fn randomize(tab: &mut [f64]) {
-    for b in tab.iter_mut() {
-        *b = thread_rng().gen_range(-1., 1.);
-    }
-}
+use std::fs::File;
+use std::io::prelude::*;
 
 fn randomize_layer(tab: &mut [f64]) {
     for b in tab.iter_mut() {
         *b = thread_rng().gen_range(0., 1.);
     }
 }
+fn bet(output: &[f64]) -> u8 {
+    let mut bet = (0, 0.);
 
-fn cost_function(neurone_value: f64, goal: f64) -> f64 {
-    (neurone_value - goal).powi(2)
-}
-
-fn cost_function_derivative(neurone_value: f64, goal: f64) -> f64 {
-    2. * (neurone_value - goal)
-}
-
-fn z(layer1: &[f64], neurone_nb: usize, biais: &[f64], weight: &[f64]) -> f64 {
-    let mut res = biais[neurone_nb];
-    for i in 0..layer1.len() {
-        res += weight[neurone_nb * layer1.len() + i] * layer1[i];
+    for i in 0..output.len() {
+        if bet.1 < output[i] {
+            bet = (i, output[i]);
+        }
     }
-    res
+    bet.0 as u8
 }
 
-fn layer_compute(layer1: &[f64], layer2: &mut [f64], biais: &[f64], weight: &[f64]) {
-    for i in 0..layer2.len() {
-        layer2[i] = sigmoid(z(&layer1, i, &biais, &weight));
+fn calculate_loss(
+    test_images: &[[f64; 784]],
+    test_labels: &[u8],
+    neural_network: &neural_network::NeuralNet,
+) -> f64 {
+    let mut tested = 0;
+    let mut failed = 0;
+
+    for k in 0..test_images.len() / 2 {
+        let output = neural_network.compute(&test_images[k]).unwrap();
+        let output = output.last().unwrap();
+        tested += 1;
+        if bet(output) != test_labels[k] {
+            failed += 1;
+        }
     }
+    failed as f64 / tested as f64
 }
 
-fn backprop_biais(
-    d_co_neu: f64,
-    neurone_nb: usize,
-    d_cost_biais: &mut [f64],
-    layer_prec: &[f64],
-    biais: &[f64],
-    weight: &[f64],
-) {
-    d_cost_biais[neurone_nb] =
-        sigmoid_derivative(z(&layer_prec, neurone_nb, &biais, &weight)) * d_co_neu;
+fn save_neural_network(path: &str, neural_network: &neural_network::NeuralNet) {
+    println!("Save to : {}", path);
+    let encoded: Vec<u8> = bincode::serialize(&neural_network).unwrap();
+    let mut buffer = File::create(path).unwrap();
+
+    buffer.write_all(&encoded).unwrap();
+    println!("Done !");
 }
 
-fn backprop_weight(
-    d_co_neu: f64,
-    neu_nb: usize,
-    d_cost_weight: &mut [f64],
-    layer_prec: &[f64],
-    biais: &[f64],
-    weight: &[f64],
-) {
-    for j in 0..layer_prec.len() {
-        d_cost_weight[neu_nb * layer_prec.len() + j] =
-            layer_prec[j] * sigmoid_derivative(z(&layer_prec, neu_nb, &biais, &weight)) * d_co_neu;
-    }
-}
+fn load_neural_network(path: &str) -> neural_network::NeuralNet {
+    let mut buffer = File::open(path).unwrap();
+    let mut neural_network = Vec::new();
 
-fn backprop_prec_layer(
-    d_co_neu: f64,
-    neu_nb: usize,
-    d_cost_prec_layer: &mut [f64],
-    layer_prec: &[f64],
-    biais: &[f64],
-    weight: &[f64],
-) {
-    for j in 0..layer_prec.len() {
-        d_cost_prec_layer[j] += weight[neu_nb * layer_prec.len() + j]
-            * sigmoid_derivative(z(&layer_prec, neu_nb, &biais, &weight))
-            * d_co_neu;
-    }
+    buffer.read_to_end(&mut neural_network).unwrap();
+    bincode::deserialize(&neural_network).unwrap()
 }
 
 fn main() {
-    let mut layer_value1: [f64; 784] = [0.0; 784];
-    let mut layer_value2: [f64; 16] = [0.0; 16];
-    let mut layer_value3: [f64; 16] = [0.0; 16];
-    let mut layer_value4: [f64; 10] = [0.0; 10];
-
-    let mut biais2: [f64; 16] = [0.0; 16];
-    let mut biais3: [f64; 16] = [0.0; 16];
-    let mut biais4: [f64; 10] = [0.0; 10];
-
-    let mut weight1_2: [f64; 12544] = [0.0; 12544];
-    let mut weight2_3: [f64; 256] = [0.0; 256];
-    let mut weight3_4: [f64; 160] = [0.0; 160];
-
-    randomize(&mut biais2);
-    randomize(&mut biais3);
-    randomize(&mut biais4);
-
-    randomize(&mut weight1_2);
-    randomize(&mut weight2_3);
-    randomize(&mut weight3_4);
-
-    randomize_layer(&mut layer_value1);
-
     let images = read_idx::read_images("./data/train-images-idx3-ubyte").unwrap();
     let labels = read_idx::read_labels("./data/train-labels-idx1-ubyte").unwrap();
 
-    for n in 0..1 {
+    let test_images = read_idx::read_images("./data/t10k-images-idx3-ubyte").unwrap();
+    let test_labels = read_idx::read_labels("./data/t10k-labels-idx1-ubyte").unwrap();
+
+    let mut rng = thread_rng();
+    let mut nn = neural_network::NeuralNet::new(&[784, 50, 28, 50, 784]);
+    let mut nn = load_neural_network("model-03500");
+    /*let mut random_layer = [0.0; 784];
+    for n in 0..10000 {
         for i in 0..28 {
             for j in 0..28 {
                 print!(
@@ -121,7 +80,7 @@ fn main() {
                     if images[n][i * 28 + j] > 0.66 {
                         "X"
                     } else if images[n][i * 28 + j] > 0.33 {
-                        "x"
+                        "-"
                     } else {
                         " "
                     }
@@ -129,156 +88,70 @@ fn main() {
             }
             print!("\n");
         }
-        println!("{}", labels[n]);
+        let output = nn.compute(&images[n]).unwrap();
+        let output = output.last().unwrap();
+
+        let bet = bet(output);
+        println!("bet: {}, {:#?}", bet, output);
+    }*/
+    for a in 0..images.len() / STEP_SIZE {
+        println!("step {}", a);
+        if a % 1 == 0 {
+            let test_image = &test_images[rng.gen_range(0, test_images.len())];
+            let output = nn.compute(test_image).unwrap();
+            let output = output.last().unwrap();
+
+            for i in 0..28 {
+                for j in 0..28 {
+                    print!(
+                        "{} ",
+                        if output[i * 28 + j] > 0.50 {
+                            "X"
+                        } else if output[i * 28 + j] > 0.40 {
+                            "-"
+                        } else {
+                            " "
+                        }
+                    );
+                }
+                print!("\n");
+            }
+            println!("\n");
+            for i in 0..28 {
+                for j in 0..28 {
+                    print!(
+                        "{} ",
+                        if test_image[i * 28 + j] > 0.66 {
+                            "X"
+                        } else if test_image[i * 28 + j] > 0.33 {
+                            "-"
+                        } else {
+                            " "
+                        }
+                    );
+                }
+                print!("\n");
+            }
+        }
+
+        if a % 100 == 0 {
+            save_neural_network(&format!("./train/model-{:05}", a), &nn);
+        }
+
+        let mut d_cost_list = Vec::new();
+
+        for k in 0..STEP_SIZE {
+            let mut goal: [f64; 10] = [0.0; 10];
+            goal[labels[a * STEP_SIZE + k] as usize] = 1.0;
+            d_cost_list.push(
+                nn.backprop(&images[a * STEP_SIZE + k], &images[a * STEP_SIZE + k])
+                    .unwrap(),
+            );
+        }
+        nn.apply_d_cost_net(d_cost_list.average().unwrap()).unwrap();
     }
-
-    
-    layer_compute(&images[0], &mut layer_value2, &biais2, &weight1_2);
-    layer_compute(&layer_value2, &mut layer_value3, &biais3, &weight2_3);
-    layer_compute(&layer_value3, &mut layer_value4, &biais4, &weight3_4);
-
-    let mut goal: [f64; 10] = [0.0; 10];
-    goal[labels[0] as usize] = 1.0;
-
-    for i in 0..layer_value4.len() {
-        println!(
-            "{}: {:.3}, goal: {:.3}, cost: {:.3}",
-            i,
-            layer_value4[i],
-            goal[i],
-            cost_function(layer_value4[i], goal[i])
-        );
-    }
-
-    let mut d_cost_biais4 = [0.0; 10];
-    let mut d_cost_weight3_4 = [0.0; 160];
-    let mut d_cost_layer3 = [0.0; 16];
-    for i in 0..layer_value4.len() {
-        backprop_biais(
-            cost_function_derivative(layer_value4[i], goal[i]),
-            i,
-            &mut d_cost_biais4,
-            &layer_value3,
-            &biais4,
-            &weight3_4,
-        );
-        backprop_weight(
-            cost_function_derivative(layer_value4[i], goal[i]),
-            i,
-            &mut d_cost_weight3_4,
-            &layer_value3,
-            &biais4,
-            &weight3_4,
-        );
-        backprop_prec_layer(
-            cost_function_derivative(layer_value4[i], goal[i]),
-            i,
-            &mut d_cost_layer3,
-            &layer_value3,
-            &biais4,
-            &weight3_4,
-        );
-    }
-
-    let mut d_cost_biais3 = [0.0; 16];
-    let mut d_cost_weight2_3 = [0.0; 256];
-    let mut d_cost_layer2 = [0.0; 16];
-    for i in 0..layer_value3.len() {
-        backprop_biais(
-            d_cost_layer3[i],
-            i,
-            &mut d_cost_biais3,
-            &layer_value2,
-            &biais3,
-            &weight2_3,
-        );
-        backprop_weight(
-            d_cost_layer3[i],
-            i,
-            &mut d_cost_weight2_3,
-            &layer_value2,
-            &biais3,
-            &weight2_3,
-        );
-        backprop_prec_layer(
-            d_cost_layer3[i],
-            i,
-            &mut d_cost_layer2,
-            &layer_value2,
-            &biais3,
-            &weight2_3,
-        );
-    }
-
-    let mut d_cost_biais2 = [0.0; 16];
-    let mut d_cost_weight1_2 = [0.0; 12544];
-    for i in 0..layer_value2.len() {
-        backprop_biais(
-            d_cost_layer2[i],
-            i,
-            &mut d_cost_biais2,
-            &layer_value3,
-            &biais2,
-            &weight1_2,
-        );
-        backprop_weight(
-            d_cost_layer2[i],
-            i,
-            &mut d_cost_weight1_2,
-            &layer_value1,
-            &biais2,
-            &weight1_2,
-        );
-    }
-
-    for i in 0..biais4.len() {
-        biais4[i] -= d_cost_biais4[i];
-    }
-
-    for i in 0..biais3.len() {
-        biais3[i] -= d_cost_biais3[i];
-    }
-
-    for i in 0..biais2.len() {
-        biais2[i] -= d_cost_biais2[i];
-    }
-    println!("biais modify\n");
-
-    layer_compute(&layer_value3, &mut layer_value4, &biais4, &weight3_4);
-
-    for i in 0..layer_value4.len() {
-        println!(
-            "{}: {:.3}, goal: {:.3}, cost: {:.3}",
-            i,
-            layer_value4[i],
-            goal[i],
-            cost_function(layer_value4[i], goal[i])
-        );
-    }
-
-    for i in 0..weight3_4.len() {
-        weight3_4[i] -= d_cost_weight3_4[i];
-    }
-
-    for i in 0..weight2_3.len() {
-        weight2_3[i] -= d_cost_weight2_3[i];
-    }
-
-    for i in 0..weight1_2.len() {
-        weight1_2[i] -= d_cost_weight1_2[i];
-    }
-
-    println!("weight modify\n");
-    layer_compute(&layer_value3, &mut layer_value4, &biais4, &weight3_4);
-
-    for i in 0..layer_value4.len() {
-        println!(
-            "{}: {:.3}, goal: {:.3}, cost: {:.3}",
-            i,
-            layer_value4[i],
-            goal[i],
-            cost_function(layer_value4[i], goal[i])
-        );
-    }
-    println!("step ++\n");
+    save_neural_network(
+        &format!("./train/model-{:05}", images.len() / STEP_SIZE),
+        &nn,
+    );
 }
---
+++ src/neural_network.rs
@@ -0,0 +1,289 @@
+use rand::prelude::*;
+use serde::{Deserialize, Serialize};
+use std::io::{Error, ErrorKind};
+
+const PROPORTION_CHANGE_D_COST: f64 = 1.;
+
+fn randomize(tab: &mut [f64]) {
+    for b in tab.iter_mut() {
+        *b = thread_rng().gen_range(-1., 1.);
+    }
+}
+
+fn sigmoid(x: f64) -> f64 {
+    1. / (1. + (-x).exp())
+}
+
+fn sigmoid_derivative(x: f64) -> f64 {
+    let tmp = (-x).exp();
+    tmp / (1. + tmp).powi(2)
+}
+
+fn cost_function_derivative(neurone_value: f64, goal: f64) -> f64 {
+    2. * (neurone_value - goal)
+}
+
+fn z(layer1: &[f64], neurone_nb: usize, biais: &[f64], weight: &[f64]) -> f64 {
+    let mut res = biais[neurone_nb];
+    for i in 0..layer1.len() {
+        res += weight[neurone_nb * layer1.len() + i] * layer1[i];
+    }
+    res
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct NeuralNet {
+    layer_sizes: Vec<usize>,
+    biais: Vec<Vec<f64>>,
+    weights: Vec<Vec<f64>>,
+}
+
+pub struct DCostNeuralNet {
+    pub layer_sizes: Vec<usize>,
+    pub biais: Vec<Vec<f64>>,
+    pub weights: Vec<Vec<f64>>,
+}
+
+impl DCostNeuralNet {
+    pub fn new(layers: &[usize]) -> Self {
+        let mut biais = Vec::new();
+        let mut weights = Vec::new();
+
+        for i in 0..layers.len() - 1 {
+            let mut layer_biais = Vec::new();
+            let mut layer_weights = Vec::new();
+
+            layer_weights.resize(layers[i] * layers[i + 1], 0.);
+            weights.push(layer_weights);
+
+            layer_biais.resize(layers[i + 1], 0.);
+            biais.push(layer_biais);
+        }
+
+        Self {
+            layer_sizes: Vec::from(layers),
+            biais,
+            weights,
+        }
+    }
+}
+
+pub trait AverageDCost {
+    fn average(&self) -> Result<DCostNeuralNet, Error>;
+}
+
+impl AverageDCost for Vec<DCostNeuralNet> {
+    fn average(&self) -> Result<DCostNeuralNet, Error> {
+        let mut average = if let Some(first) = self.first() {
+            DCostNeuralNet::new(&first.layer_sizes)
+        } else {
+            return Err(Error::new(
+                ErrorKind::Other,
+                "Can't make an average of an empty DCostNeuralNet set",
+            ));
+        };
+
+        for d_cost_neural_net in self {
+            if d_cost_neural_net.biais.len() != average.biais.len()
+                || d_cost_neural_net.weights.len() != average.weights.len()
+            {
+                return Err(Error::new(ErrorKind::Other, "Wrong network size"));
+            }
+            for i in 0..average.biais.len() {
+                if average.biais[i].len() != d_cost_neural_net.biais[i].len() {
+                    return Err(Error::new(ErrorKind::Other, "Wrong network size"));
+                }
+                for j in 0..average.biais[i].len() {
+                    average.biais[i][j] += d_cost_neural_net.biais[i][j] / self.len() as f64;
+                }
+            }
+            for i in 0..average.weights.len() {
+                if average.weights[i].len() != d_cost_neural_net.weights[i].len() {
+                    return Err(Error::new(ErrorKind::Other, "Wrong network size"));
+                }
+                for j in 0..average.weights[i].len() {
+                    average.weights[i][j] += d_cost_neural_net.weights[i][j] / self.len() as f64;
+                }
+            }
+        }
+        Ok(average)
+    }
+}
+
+impl NeuralNet {
+    pub fn new(layers: &[usize]) -> Self {
+        let mut biais = Vec::new();
+        let mut weights = Vec::new();
+
+        for i in 0..layers.len() - 1 {
+            let mut layer_biais = Vec::new();
+            let mut layer_weights = Vec::new();
+
+            layer_weights.resize(layers[i] * layers[i + 1], 0.);
+            randomize(&mut layer_weights);
+            weights.push(layer_weights);
+
+            layer_biais.resize(layers[i + 1], 0.);
+            randomize(&mut layer_biais);
+            biais.push(layer_biais);
+        }
+
+        Self {
+            layer_sizes: Vec::from(layers),
+            biais,
+            weights,
+        }
+    }
+
+    pub fn compute(&self, input: &[f64]) -> Result<Vec<Vec<f64>>, Error> {
+        if input.len() != self.layer_sizes[0] {
+            return Err(Error::new(
+                ErrorKind::Other,
+                format!(
+                    "Wrong Input Size: {}, expected: {}",
+                    input.len(),
+                    self.layer_sizes[0]
+                ),
+            ));
+        }
+
+        let mut layer_values: Vec<Vec<f64>> = vec![Vec::from(input)];
+
+        for i in 0..self.layer_sizes.len() - 1 {
+            let mut next_layer = Vec::new();
+            for j in 0..self.layer_sizes[i + 1] {
+                next_layer.push(sigmoid(z(
+                    &layer_values[i],
+                    j,
+                    &self.biais[i],
+                    &self.weights[i],
+                )));
+            }
+            layer_values.push(next_layer);
+        }
+
+        Ok(layer_values)
+    }
+
+    fn backprop_biais(
+        d_co_neu: f64,
+        neurone_nb: usize,
+        d_cost_biais: &mut [f64],
+        layer_prec: &[f64],
+        biais: &[f64],
+        weight: &[f64],
+    ) {
+        d_cost_biais[neurone_nb] =
+            sigmoid_derivative(z(&layer_prec, neurone_nb, &biais, &weight)) * d_co_neu;
+    }
+
+    fn backprop_weight(
+        d_co_neu: f64,
+        neu_nb: usize,
+        d_cost_weight: &mut [f64],
+        layer_prec: &[f64],
+        biais: &[f64],
+        weight: &[f64],
+    ) {
+        for j in 0..layer_prec.len() {
+            d_cost_weight[neu_nb * layer_prec.len() + j] = layer_prec[j]
+                * sigmoid_derivative(z(&layer_prec, neu_nb, &biais, &weight))
+                * d_co_neu;
+        }
+    }
+
+    fn backprop_prec_layer(
+        d_co_neu: f64,
+        neu_nb: usize,
+        d_cost_prec_layer: &mut [f64],
+        layer_prec: &[f64],
+        biais: &[f64],
+        weight: &[f64],
+    ) {
+        for j in 0..layer_prec.len() {
+            d_cost_prec_layer[j] += weight[neu_nb * layer_prec.len() + j]
+                * sigmoid_derivative(z(&layer_prec, neu_nb, &biais, &weight))
+                * d_co_neu;
+        }
+    }
+
+    pub fn backprop(
+        &mut self,
+        input: &[f64],
+        output_goal: &[f64],
+    ) -> Result<DCostNeuralNet, Error> {
+        let layer_values = self.compute(input)?;
+        let mut d_cost_neural_net = DCostNeuralNet::new(&self.layer_sizes);
+
+        let mut d_co_prec: Vec<f64> = layer_values
+            .last()
+            .unwrap()
+            .iter()
+            .enumerate()
+            .map(|(i, x)| cost_function_derivative(*x, output_goal[i]))
+            .collect();
+
+        for k in 1..self.layer_sizes.len() {
+            let layer_nb = self.layer_sizes.len() - 1 - k;
+
+            let mut new_d_co = Vec::new();
+            new_d_co.resize(self.layer_sizes[layer_nb], 0.);
+
+            for i in 0..d_co_prec.len() {
+                Self::backprop_biais(
+                    d_co_prec[i],
+                    i,
+                    &mut d_cost_neural_net.biais[layer_nb],
+                    &layer_values[layer_nb],
+                    &self.biais[layer_nb],
+                    &self.weights[layer_nb],
+                );
+                Self::backprop_weight(
+                    d_co_prec[i],
+                    i,
+                    &mut d_cost_neural_net.weights[layer_nb],
+                    &layer_values[layer_nb],
+                    &self.biais[layer_nb],
+                    &self.weights[layer_nb],
+                );
+                if layer_nb > 0 {
+                    Self::backprop_prec_layer(
+                        d_co_prec[i],
+                        i,
+                        &mut new_d_co,
+                        &layer_values[layer_nb],
+                        &self.biais[layer_nb],
+                        &self.weights[layer_nb],
+                    );
+                }
+            }
+            d_co_prec = new_d_co;
+        }
+        Ok(d_cost_neural_net)
+    }
+
+    pub fn apply_d_cost_net(&mut self, d_cost_neural_net: DCostNeuralNet) -> Result<(), Error> {
+        if self.biais.len() != d_cost_neural_net.biais.len()
+            || self.weights.len() != d_cost_neural_net.weights.len()
+        {
+            return Err(Error::new(ErrorKind::Other, "Wrong network size"));
+        }
+        for i in 0..self.biais.len() {
+            if self.biais[i].len() != d_cost_neural_net.biais[i].len() {
+                return Err(Error::new(ErrorKind::Other, "Wrong network size"));
+            }
+            for j in 0..self.biais[i].len() {
+                self.biais[i][j] -= d_cost_neural_net.biais[i][j] * PROPORTION_CHANGE_D_COST;
+            }
+        }
+        for i in 0..self.weights.len() {
+            if self.weights[i].len() != d_cost_neural_net.weights[i].len() {
+                return Err(Error::new(ErrorKind::Other, "Wrong network size"));
+            }
+            for j in 0..self.weights[i].len() {
+                self.weights[i][j] -= d_cost_neural_net.weights[i][j] * PROPORTION_CHANGE_D_COST;
+            }
+        }
+        Ok(())
+    }
+}
